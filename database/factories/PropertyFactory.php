7cd7b6261ee870e9fce63d348a8ddcefa5afe144
thibault <thibaultfaivre40@gmail.com>
2022-09-16 17:50:54 +0200

    first commit

---
+++ database/factories/PropertyFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Property>
+ */
+class PropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'surface' => fake()->randomNumber(2),
+            'amount' => fake()->randomFloat(2,200,2000),
+            'status' => 'available',
+            'user_id' => \App\Models\User::first()->id
+            
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
